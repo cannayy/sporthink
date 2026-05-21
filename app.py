d1fb6bbb8238009f446cb5504ed421f1239d84ce
cannayy <isacan.ayy@gmail.com>
2026-05-21 22:01:20 +0300

    MAIL_FROM ayrı env var

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', '587'))
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_FROM', os.getenv('MAIL_USERNAME'))
 app.config['MAIL_TIMEOUT'] = 10
 
 from utils.mail import init_mail
